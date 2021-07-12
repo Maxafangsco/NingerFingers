e9825c92c8bbc7b09d59ff282cf1977cb5a71285
maxafangsco <maxafangsco@gmail.com>
2021-07-12 18:07:04 +0400

    first commit for the final game

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# NingerFingers
-A simple game that measures your reaction time using Vue
+# modal-project
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+<h1>Ninger  Reaction Timer</h1>
+<button @click="start" :disabled="isplaying ">Play</button>
+<Block v-if="isplaying" :delay="delay" @end="endGame" />
+<!-- <p v-if="showResults">Reaction time : {{score}} ms</p> -->
+<Results v-if="showResults" :score="score"/>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
+
+export default {
+  name: 'App',
+  components: {
+   Block,
+   Results
+  },
+   data() {
+    return {
+     isplaying: false,
+     delay: null,
+     score: null,
+     showResults: false,
+    }
+  },
+
+  methods:{
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isplaying = true
+       this.showResults = false
+    },
+    endGame(reactionTime){
+      this.score= reactionTime
+      this.isplaying = false
+      this.showResults = true
+
+    }
+  },
+
+ 
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
